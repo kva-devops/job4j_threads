b9ff11999178b5c14aa9ce4fa0eb0f8955679e81
vladimirKutyavin <kva.devops@gmail.com>
2021-07-31 09:45:37 +0400

    3. Прерывание нити [#1019 #183506]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 public class ConsoleProgress implements Runnable {
 
-    int count = 0;
-
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
@@ -16,29 +14,22 @@ public class ConsoleProgress implements Runnable {
 
     @Override
     public void run() {
+
+        int count = 0;
+
         while (!Thread.currentThread().isInterrupted()) {
             try {
-                System.out.print("\r loading : " + process());
+                System.out.print("\r loading : " + process(new ArrayList<>(
+                        List.of('-', '\\', '|', '/')), count));
                 Thread.sleep(350);
+                count = (count < 3) ? count + 1 : 0;
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
         }
     }
 
-    private char process() {
-        char buff;
-        List<Character> characterList = new ArrayList<>(
-                List.of(
-                        '-', '\\', '|', '/'
-                )
-        );
-        buff = characterList.get(count);
-        if (count < 3) {
-            count++;
-        } else {
-            count = 0;
-        }
-        return buff;
+    private char process(List<Character> list, int count) {
+        return list.get(count);
     }
 }
